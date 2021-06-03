29172ec7424e447e555d100e1476ae7103361914
Maximilian Kerling <maximilian.kerling@gmail.com>
2021-06-03 20:46:24 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Maximilian Kerling on 03.06.21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
